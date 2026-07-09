c5509efdb16478699b4903369910d660b4eb258b
Maksim426 <maksimpoliebko100@gmail.com>
2026-07-09 14:11:04 +0300

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,28 @@
     var states = { god: false, shield: false };
     
     var printHelp = function() {
-        Log.info("\n[blue]=== ULTRASCRIPT V21.0 - АКТИВИРОВАН ===");
-        Log.info("[blue]creative[/blue]     : Ресурсы (песочница)");
-        Log.info("[blue]editor[/blue]       : Режим редактора");
-        Log.info("[blue]god[/blue]          : Бессмертие");
-        Log.info("[blue]instant[/blue]      : Мгновенная стройка");
-        Log.info("[blue]shield[/blue]       : Бесконечный щит");
-        Log.info("[blue]win[/blue]          : Победа в волне");
-        Log.info("[blue]kill[/blue]         : Убить врагов");
-        Log.info("[blue]fill[/blue]         : Заполнить ядро");
-        Log.info("[blue]dump[/blue]         : Очистить ядро");
-        Log.info("[blue]heal[/blue]         : Исцелить постройки");
-        Log.info("[blue]spawn[/blue]        : Спавн юнитов");
-        Log.info("[blue]off[/blue]          : Сброс настроек");
-        Log.info("[white]Используй [blue]help[/blue] для повторного вывода меню.[/white]");
+        Log.info("\n[blue]==========================================");
+        Log.info("[blue]||  [white]ULTRASCRIPT V21.1 - ПАНЕЛЬ УПРАВЛЕНИЯ[/white]  [blue]||");
+        Log.info("[blue]==========================================[/blue]");
+        Log.info("[blue] creative [/white]: Ресурсы (песочница)");
+        Log.info("[blue] editor   [/white]: Режим редактора");
+        Log.info("[blue] god      [/white]: Бессмертие");
+        Log.info("[blue] instant  [/white]: Мгновенная стройка");
+        Log.info("[blue] shield   [/white]: Бесконечный щит");
+        Log.info("[blue] win      [/white]: Завершить волну");
+        Log.info("[blue] kill     [/white]: Уничтожить врагов");
+        Log.info("[blue] fill     [/white]: Заполнить ядро");
+        Log.info("[blue] dump     [/white]: Очистить ядро");
+        Log.info("[blue] heal     [/white]: Исцелить постройки");
+        Log.info("[blue] spawn(n) [/white]: Спавн юнита (напр. spawn('dagger'))");
+        Log.info("[blue] off      [/white]: Сброс всех систем");
+        Log.info("[blue]==========================================");
+        Log.info("[white] Используй [blue]help[/blue] или [blue]help_spawn[/blue] для помощи.[/white]");
     };
 
-    Events.on("worldLoad", printHelp);
+    Events.on(EventType.WorldLoadEvent, printHelp);
 
-    Events.on("update", function() {
+    Events.on(EventType.UnitControlEvent, function() {
         var u = Vars.player.unit();
         if (u) {
             if (states.god) u.health = u.maxHealth;
@@ -30,6 +33,13 @@
     });
 
     Object.defineProperty(scope, 'help', { get: function() { printHelp(); return ""; }, configurable: true });
+    
+    Object.defineProperty(scope, 'help_spawn', { get: function() { 
+        Log.info("\n[blue]-- ИНСТРУКЦИЯ ПО СПАВНУ --");
+        Log.info("[white]Для спавна введите: [blue]spawn('имя_юнита')[/blue]");
+        Log.info("[white]Пример: [blue]spawn('dagger')[/blue]");
+        return ""; 
+    }, configurable: true });
 
     Object.defineProperty(scope, 'creative', { get: function() { Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources; Log.info(">>> Creative: " + (Vars.state.rules.infiniteResources ? "[green]ВКЛ" : "[red]ВЫКЛ")); return ""; }, configurable: true });
     Object.defineProperty(scope, 'editor', { get: function() { Vars.state.rules.editor = !Vars.state.rules.editor; Log.info(">>> Editor: " + (Vars.state.rules.editor ? "[green]ВКЛ" : "[red]ВЫКЛ")); return ""; }, configurable: true });
@@ -43,7 +53,14 @@
         return ""; 
     }, configurable: true });
 
-    Object.defineProperty(scope, 'win', { get: function() { Vars.logic.skipWave(); return ">>> Победа!"; }, configurable: true });
+    Object.defineProperty(scope, 'win', { get: function() { 
+        Groups.unit.each(function(u) { if (u.team != Vars.player.team()) u.kill(); });
+        if (typeof Vars.spawner !== 'undefined' && Vars.spawner !== null && Vars.spawner.waves !== null) {
+            Vars.spawner.waves.clear();
+        }
+        return ">>> Волна принудительно завершена!"; 
+    }, configurable: true });
+    
     Object.defineProperty(scope, 'kill', { get: function() { Groups.unit.each(function(u) { if (u.team != Vars.player.team()) u.kill(); }); return ">>> Враги уничтожены"; }, configurable: true });
     
     Object.defineProperty(scope, 'fill', { get: function() { 
@@ -65,12 +82,8 @@
     Object.defineProperty(scope, 'heal', { get: function() { Groups.build.each(function(b) { if (b.team == Vars.player.team()) b.health = b.maxHealth; }); return ">>> Постройки исцелены"; }, configurable: true });
     Object.defineProperty(scope, 'off', { get: function() { states.god = false; states.shield = false; Vars.state.rules.infiniteResources = false; Vars.state.rules.editor = false; Vars.state.rules.buildSpeedMultiplier = 1; if(Vars.player.unit()) Vars.player.unit().shield = 0; return ">>> Системы сброшены"; }, configurable: true });
 
-    Object.defineProperty(scope, 'spawn', { get: function() { 
-        Log.info("[blue]Для спавна введите: spawn_unit('имя')[/blue]");
-        return ""; 
-    }, configurable: true });
-
-    scope.spawn_unit = function(name) {
+    scope.spawn = function(name) {
+        if (!name) { Log.info("[red]Ошибка: Введите имя юнита в скобках, напр: spawn('dagger')"); return ""; }
         var type = Vars.content.units().find(function(u) { return u.name.includes(name); });
         if (type) {
             var u = type.create(Vars.player.team());
@@ -81,4 +94,4 @@
         return "[red]Ошибка: Юнит не найден.";
     };
 })();
-                
+        
