7755b7b297e637d3da595f5400d872e5cdf682d4
Maksim426 <maksimpoliebko100@gmail.com>
2026-07-09 15:19:41 +0300

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 (function() {
     var scope = (typeof vars !== 'undefined') ? this : (function() { return this; })();
-    var states = { god: false, shield: false };
+    var states = { god: false };
     
     var printHelp = function() {
         Log.info("\n=== ULTRASCRIPT V21.0 ===");
@@ -8,7 +8,6 @@
         Log.info("editor : Редактор");
         Log.info("god : Бессмертие");
         Log.info("instant : Мгновенная стройка");
-        Log.info("shield : Бесконечный щит");
         Log.info("kill : Убить врагов");
         Log.info("fill : Заполнить ядро");
         Log.info("ammo : Заполнить все турели патронами");
@@ -21,11 +20,12 @@
 
     Events.on(EventType.WorldLoadEvent, printHelp);
 
+    // Постоянное поддержание здоровья и щита, пока включен Режим Бога
     Events.on(EventType.UnitControlEvent, function() {
         var u = Vars.player.unit();
-        if (u) {
-            if (states.god) u.health = u.maxHealth;
-            if (states.shield) u.shield = 9999;
+        if (u && states.god) {
+            u.health = u.maxHealth;
+            u.shield = 999999; // Даем огромный запас прочности щита внутри режима бога
         }
     });
 
@@ -33,15 +33,23 @@
 
     Object.defineProperty(scope, 'creative', { get: function() { Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources; return "Creative: " + (Vars.state.rules.infiniteResources ? "ON" : "OFF"); }, configurable: true });
     Object.defineProperty(scope, 'editor', { get: function() { Vars.state.rules.editor = !Vars.state.rules.editor; return "Editor: " + (Vars.state.rules.editor ? "ON" : "OFF"); }, configurable: true });
-    Object.defineProperty(scope, 'god', { get: function() { states.god = !states.god; return "God Mode: " + (states.god ? "ON" : "OFF"); }, configurable: true });
-    Object.defineProperty(scope, 'instant', { get: function() { Vars.state.rules.buildSpeedMultiplier = (Vars.state.rules.buildSpeedMultiplier == 1 ? 9999 : 1); return "Instant Build: " + (Vars.state.rules.buildSpeedMultiplier > 1 ? "ON" : "OFF"); }, configurable: true });
     
-    Object.defineProperty(scope, 'shield', { get: function() { 
-        states.shield = !states.shield; 
-        if(states.shield && Vars.player.unit()) Vars.player.unit().shield = 9999;
-        return "Shield: " + (states.shield ? "ON" : "OFF"); 
+    // Обновленный Режим Бога: макс. здоровье + мощный щит в одной команде
+    Object.defineProperty(scope, 'god', { get: function() { 
+        states.god = !states.god; 
+        var u = Vars.player.unit();
+        if (u) {
+            if (states.god) {
+                u.health = u.maxHealth;
+                u.shield = 999999;
+            } else {
+                u.shield = 0;
+            }
+        }
+        return "God Mode: " + (states.god ? "ON" : "OFF"); 
     }, configurable: true });
     
+    Object.defineProperty(scope, 'instant', { get: function() { Vars.state.rules.buildSpeedMultiplier = (Vars.state.rules.buildSpeedMultiplier == 1 ? 9999 : 1); return "Instant Build: " + (Vars.state.rules.buildSpeedMultiplier > 1 ? "ON" : "OFF"); }, configurable: true });
     Object.defineProperty(scope, 'kill', { get: function() { Groups.unit.each(function(u) { if (u.team != Vars.player.team()) u.kill(); }); return "Враги уничтожены"; }, configurable: true });
     
     Object.defineProperty(scope, 'fill', { get: function() { 
@@ -88,7 +96,15 @@
 
     Object.defineProperty(scope, 'dump', { get: function() { var c = Vars.player.unit() ? Vars.player.unit().core() : null; if(c) { c.items.clear(); return "Ядро очищено"; } return "Нет ядра"; }, configurable: true });
     Object.defineProperty(scope, 'heal', { get: function() { Groups.build.each(function(b) { if (b.team == Vars.player.team()) b.health = b.maxHealth; }); return "Постройки исцелены"; }, configurable: true });
-    Object.defineProperty(scope, 'off', { get: function() { states.god = false; states.shield = false; Vars.state.rules.infiniteResources = false; Vars.state.rules.editor = false; Vars.state.rules.buildSpeedMultiplier = 1; if(Vars.player.unit()) Vars.player.unit().shield = 0; return "Системы сброшены"; }, configurable: true });
+    
+    Object.defineProperty(scope, 'off', { get: function() { 
+        states.god = false; 
+        Vars.state.rules.infiniteResources = false; 
+        Vars.state.rules.editor = false; 
+        Vars.state.rules.buildSpeedMultiplier = 1; 
+        if(Vars.player.unit()) Vars.player.unit().shield = 0; 
+        return "Системы сброшены"; 
+    }, configurable: true });
 
     scope.spawn = function(name) {
         var type = Vars.content.units().find(function(u) { return u.name.includes(name); });
